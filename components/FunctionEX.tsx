03a33b8c8568e4e4a5b07d7340ec7b0ed3fdef1d
Võ Minh Kiệt <125539745+kiethien@users.noreply.github.com>
2023-11-14 11:12:43 +0700

    bỏ 6 nút
    bỏ đi tại chưa định hướng được homepage thế nào cho ổn

--- components/FunctionEX.tsx
+++ components/FunctionEX.tsx
@@ -3,64 +3,7 @@ import Button from './Button'
 
 const Function = () => {
   return (
-    <section className='flex flex-col gap-x-12 flexStart overflow-hidden '>
-      <div className='container flex flex-wrap gap-x-10 padding-container'>
-          <div className=' inline-flex items-center'>
-          <Button
-            type='button'
-            title='Link/URL'
-            icon='/linh_url.svg'
-            variant='btn_dark_green_big'
-          />
-          </div>
-
-          <div className=' inline-flex items-center'>
-            <Button
-              type='button'
-              title='Login'
-              icon='/user.svg'
-              variant='btn_dark_green'
-              />
-          </div>
-
-          <div className=' inline-flex items-center'>
-              <Button
-              type='button'
-              title='Login'
-              icon='/user.svg'
-              variant='btn_dark_green'
-              />
-          </div>
-        </div>
-        <div className='container flex flexBetween flexCenter padding-container'>
-        <div className=' inline-flex items-center'>
-          <Button
-            type='button'
-            title='Link/URL'
-            icon='/linh_url.svg'
-            variant='btn_gray_dark'
-          />
-          </div>
-
-          <div className=' inline-flex items-center'>
-            <Button
-              type='button'
-              title='Login'
-              icon='/user.svg'
-              variant='btn_dark_green'
-              />
-          </div>
-
-          <div className=' inline-flex items-center'>
-              <Button
-              type='button'
-              title='Login'
-              icon='/user.svg'
-              variant='btn_dark_green'
-              />
-          </div>
-        </div>
-    </section>
+    <></>
   )
 }
 
