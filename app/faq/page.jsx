f2b8ff18bbdbcaf46838092eec51462e3a2c9979
Võ Minh Kiệt <125539745+kiethien@users.noreply.github.com>
2023-11-16 11:43:53 +0700

    faq
    faq hoàn thiện

---
+++ app/faq/page.jsx
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+import Image from "next/image";
+import { QUESTIONS } from '@/constants'
+import SingleQuestion from "./SingleQuestion";
+
+function App() {
+  return (
+    <main>
+      {/* Hero Section */}
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Frequently Asked Questions About QREACTIVE</h1>
+          <p>Below we have collected answers to questions that you may have</p>
+        </div>    
+        
+        <div className="image-content">
+          <Image src="/top-img.webp" width={100} height={50} alt="Hero Image" />
+        </div>
+
+      </section>
+
+      {/* Questions Section */}
+      <div className="container">
+        <h3>Question you may have when using our web</h3>
+        <section>
+          {QUESTIONS.map((QUESTIONS) => (
+            <SingleQuestion key={QUESTIONS.id} {...QUESTIONS} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
